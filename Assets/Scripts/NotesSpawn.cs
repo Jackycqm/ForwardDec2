bdcd4c0fc9ef346f60d8395d0dbf8f005c6b84ed
Jacky Chen <qc308@nyu.edu>
2019-11-04 19:48:15 +0000

    No Comments

---
+++ Assets/Scripts/NotesSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        float songPositionSeconds;
+        //The current position of the song in seconds
+
+        float songPositionBeats;
+        //The current position of the song in beats
+        //BPM (Beats Per Minutes) is calculated
+
+        float secondsPerbeat;
+        //How long is a beat in seconds
+
+        float dsptimesong;
+        //https://docs.unity3d.com/ScriptReference/AudioSettings-dspTime.html
+        //how many seconds has passed since song started
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
