1464f94ade5a05a0066e9343e1ad3c157b953fbb
Mengqi Gao <mengqigao7@gmail.com>
2019-11-11 01:42:27 +0000

    2d enviornment, 场景可滚动

---
+++ Assets/Scripts/2d enviornment/appear.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class appear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera _theCamera;
+    private float _HalfHeight;
+  	private float _HalfWidth;
+    public float xxx;
+    //private Vector3 newlocation;
+    void Start()
+    {
+      //get camera width
+      _HalfHeight = _theCamera.orthographicSize;
+  		_HalfWidth = _HalfHeight * Screen.width / Screen.height;
+      _theCamera = GetComponent<Camera> ();
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    }
+
+    void OnTriggerEnter2D()
+    {
+      transform.position = new Vector3 (transform.position.x + _HalfWidth*2 + xxx, transform.position.y, transform.position.z);
+      //newlocation = new Vector3(transform.position.x + _HalfWidth*2 + xxx, transform.position.y, transform.position.z);
+    }
+
+}
