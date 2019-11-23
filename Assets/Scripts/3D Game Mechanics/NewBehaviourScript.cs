012bde703924fa58c477a28a5317074350fb0579
Jacky Chen <qc308@nyu.edu>
2019-11-23 20:57:50 +0000

    Jacky New System

--- Assets/Scripts/3D Game Mechanics/NewBehaviourScript.cs
+++ Assets/Scripts/3D Game Mechanics/NewBehaviourScript.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
+        
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
+        
     }
 }
